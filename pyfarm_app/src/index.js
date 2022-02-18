d04735e686a184566625f7c0292483c99aff8f7b
Kevin Williams <kevin@ktech.industries>
2022-02-18 07:06:49 -0600

    Web App
    Initial setup for react app

--- pyfarm_app/src/index.js
+++ pyfarm_app/src/index.js
@@ -1,14 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { SnackbarProvider } from "notistack";
+import Slide from "@mui/material/Slide";
+
+import App from "./App";
+import store from "./redux/Store";
+
+import reportWebVitals from "./reportWebVitals";
+
+import "./index.css";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#43a047",
+      light: "#76d275",
+      dark: "#00701a",
+    },
+    secondary: {
+      main: "#cddc39",
+      light: "#ffff6e",
+      dark: "#99aa00",
+    },
+  },
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <SnackbarProvider
+        maxSnack={3}
+        autoHideDuration={2000}
+        TransitionComponent={Slide}
+        anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+      >
+        <App />
+      </SnackbarProvider>
+    </ThemeProvider>
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
